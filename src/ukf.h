eb893158d60d67cdd9e7e6b5b4649e94d0da0ecd
Bryan Rathos <bryanrathos@gmail.com>
2017-08-11 17:04:54 +0530

    Added NIS variables for laser and radar

--- src/ukf.h
+++ src/ukf.h
@@ -68,6 +68,12 @@ class UKF
     ///* Sigma point spreading parameter
     double lambda_;
 
+    ///* the current NIS for radar
+    double NIS_radar_;
+
+    ///* the current NIS for laser
+    double NIS_laser_;
+
 
     /**
     * Constructor
